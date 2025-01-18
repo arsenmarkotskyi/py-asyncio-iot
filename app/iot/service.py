3c63f936eddb2e4e81461302cb7a62cbbb3c8ad3
Arsen Markotskyi <arsen.markotskui@gmail.com>
2025-01-18 15:54:03 +0200

    "solution"

--- app/iot/service.py
+++ app/iot/service.py
@@ -1,5 +1,7 @@
+import asyncio
 import random
 import string
+from collections import defaultdict
 from typing import Protocol
 
 from .message import Message, MessageType
@@ -26,24 +28,32 @@ class IOTService:
     def __init__(self) -> None:
         self.devices: dict[str, Device] = {}
 
-    def register_device(self, device: Device) -> str:
-        device.connect()
+    async def register_device(self, device: Device) -> str:
+        await device.connect()
         device_id = generate_id()
         self.devices[device_id] = device
         return device_id
 
-    def unregister_device(self, device_id: str) -> None:
-        self.devices[device_id].disconnect()
+    async def unregister_device(self, device_id: str) -> None:
+        await self.devices[device_id].disconnect()
         del self.devices[device_id]
 
     def get_device(self, device_id: str) -> Device:
         return self.devices[device_id]
 
-    def run_program(self, program: list[Message]) -> None:
+    async def run_program(self, program: list[Message]) -> None:
         print("=====RUNNING PROGRAM======")
+        device_programs = defaultdict(list)
         for msg in program:
-            self.send_msg(msg)
+            device_programs[msg.device_id].append(msg)
         print("=====END OF PROGRAM======")
+        await asyncio.gather(
+            *(self._run_device_program(device_id, msgs) for device_id, msgs in device_programs.items())
+        )
 
-    def send_msg(self, msg: Message) -> None:
-        self.devices[msg.device_id].send_message(msg.msg_type, msg.data)
+    async def _run_device_program(self, device_id: str, messages: list[Message]) -> None:
+        for msg in messages:
+            await self.send_msg(msg)
+
+    async def send_msg(self, msg: Message) -> None:
+        await self.devices[msg.device_id].send_message(msg.msg_type, msg.data)
